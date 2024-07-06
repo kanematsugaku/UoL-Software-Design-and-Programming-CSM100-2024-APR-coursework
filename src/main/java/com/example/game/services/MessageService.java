ae1d0ee5d4afcff5e8e5b40c93c78563bb5dd4ef
Kanematsu Gaku <kanematsu.gaku@gmail.com>
2024-07-07 01:17:24 +0900

    refactor

--- src/main/java/com/example/game/services/MessageService.java
+++ src/main/java/com/example/game/services/MessageService.java
@@ -6,24 +6,15 @@ import com.example.game.util.PrintUtil;
 @Service
 public class MessageService {
     public void showWelcomeMessage() {
-        PrintUtil.printLine("""
-                ##########################
-                ## Welcome to the game! ##
-                ##########################""");
+        PrintUtil.printImportantMessage("Welcome to the game!");
     }
 
     public void showClosingMessage() {
-        PrintUtil.printLine("""
-                ############################
-                ## Thank you for playing! ##
-                ############################""");
+        PrintUtil.printImportantMessage("Thank you for playing!");
     }
 
     public void showExceptionMessage(Exception e) {
-        PrintUtil.printLine("""
-                ##########################
-                ## An error occurred... ##
-                ##########################""");
+        PrintUtil.printImportantMessage("An error occurred...");
         PrintUtil.printLine(e.getMessage());
     }
 }
--- src/main/java/com/example/game/util/PrintUtil.java
+++ src/main/java/com/example/game/util/PrintUtil.java
@@ -1,13 +1,26 @@
 package com.example.game.util;
 
 public class PrintUtil {
-    public static void printLine(String arg) {
-      System.out.println(arg);
-    }
+  /**
+   * Print arg in a line
+   */
+  public static void printLine(String arg) {
+    System.out.println(arg);
+  }
 
-    public static void printLines(String... args) {
-        for (String arg : args) {
-          printLine(arg);
-        }
+  /**
+   * Print args in lines
+   */
+  public static void printLines(String... args) {
+    for (String arg : args) {
+      printLine(arg);
     }
+  }
+
+  /**
+   * Print arg in a box
+   */
+  public static void printImportantMessage(String arg) {
+    printLines("#".repeat(arg.length() + 6), "## " + arg + " ##", "#".repeat(arg.length() + 6));
+  }
 }
